c2a97b26f2a84e7bc01703848c3c9edbe32cea63
Javascript <garvkadia@gmail.com>
2024-05-28 10:07:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Main, Navigator, Sidebar } from "./layout/index"
+import { useGlobalContext } from "./context/context"
+
+function App() {
+  const { state } = useGlobalContext()
+
+  return (
+    <div className="App">
+      <Navigator />
+      <Sidebar isShowing={state.showSidebar} />
+      <Main />
+    </div>
+    
+  )
+}
+
+export default App
