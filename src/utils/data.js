aeed3a8e787fe1aef82686f60a6e0ba2847f2d92
Javascript <garvkadia@gmail.com>
2024-05-28 10:08:28 +0530

    commit

--- src/utils/data.js
+++ src/utils/data.js
@@ -5,7 +5,7 @@ export const data = {
   companyName: "Sneaker Company",
   productName: "Fall Limited Edition Sneakers",
   productDescription:
-    "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.",
+    "These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they will withstand everything the weather can offer.",
   productPrice: 250,
   isOnSale: true,
   salePercent: 0.5,
